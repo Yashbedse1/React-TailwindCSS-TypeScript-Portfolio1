34310c23a62763b8f59cf855180fb94ba686b3ba
Yashbedse1 <yash@oxaralondon.com>
2023-06-22 14:16:20 +0100

    Experience component changes

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { connect } from 'react-redux';
+import {IoMdArrowDroprightCircle, IoMdArrowDropleftCircle} from 'react-icons/io';
 
 interface ExperienceData {
   id: string;
@@ -10,27 +11,55 @@ interface ExperienceData {
 }
 
 const Experience = ({ experienceData }: { experienceData: ExperienceData[] }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
+  const handleNextExperience = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === experienceData.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handlePreviousExperience = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? experienceData.length - 1 : prevIndex - 1));
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex === experienceData.length - 1 ? 0 : prevIndex + 1));
+    }, 3000); 
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [experienceData]);
+
+  const currentExperience = experienceData[currentIndex];
   return (
     <div id="experience" className='flex flex-col justify-center items-center max-w-full w-full mt-auto mx-auto py-16 px-4 bg-[#0A375D] text-white'>
-      <p className='text-4xl font-bold p-4'>Experience</p>
-      {experienceData.map((experience, index) => (
-        <div key={index} className='max-w-[1240px]'>
-          <h1 className='md:text-3xl sm:text-2xl text-xl font-bold md:py-6'>{experience.title} - {experience.company} {experience.duration}</h1>
-          <div className='flex justify-left items-left'>
-            <ul className='md:text-xl sm:text-md text-sm font-medium py-4'>
-              {experience.responsibilities.map((responsibility, responsibilityIndex) => (
-                <li key={responsibilityIndex} className='pb-2'>
-                   <span className="color-black mr-2">&bull; </span>{responsibility}</li>
-              ))}
-            </ul>
+      <p className='text-4xl font-bold mb-4'>Experience</p>
+      <div className='w-full'>
+        <div className='flex justify-between items-center'>
+          <div className="flex justify-start">
+            <button onClick={handlePreviousExperience}>
+              <IoMdArrowDropleftCircle size={30} />
+            </button>
+          </div>
+          <ul className='md:text-xl sm:text-md text-sm font-medium py-4 max-w-[1240px] border-black border-4 p-4'>
+          <h1 className='md:text-3xl sm:text-2xl text-xl font-bold md:py-6'>{currentExperience.title} - {currentExperience.company} {currentExperience.duration}</h1>
+            {currentExperience.responsibilities.map((responsibility, responsibilityIndex) => (
+              <li key={responsibilityIndex} className='pb-2'>
+                <span className="color-black mr-2">&bull; </span>{responsibility}
+              </li>
+            ))}
+          </ul>
+          <div className="flex justify-end">
+            <button onClick={handleNextExperience} >
+              <IoMdArrowDroprightCircle size={30} />
+            </button>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
-
+    
 const mapStateToProps = (state: { experienceData: any; }) => {
   return {
     experienceData: state.experienceData,
